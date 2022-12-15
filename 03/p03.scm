0a6cddc25758a679e1d7e2cefab5f0f6bd4a817b
Volker Sobek <reklov@live.com>
2022-12-15 22:54:50 +0100

    03: Completed

--- 03/p03.scm
+++ 03/p03.scm
@@ -4,6 +4,7 @@
 
 (use-modules (ice-9 textual-ports)
              (ice-9 control)
+             (ice-9 match)
              (srfi srfi-1)
              (rnrs base))
 
@@ -26,42 +27,85 @@
       (loop (1+ i)))))
 
 (define (get-priority rucksack)
-  ;(format #t "current rucksack:\n~a\n" rucksack)
+  ;; (format #t "current rucksack:\n~a\n" rucksack)
   (let* ((content (string->list rucksack))
          (seen-chars (make-vector 123 #f))
          (len/2 (/ (length content) 2)))
-
     (call/ec                           ; allows to escape from fold by
      (lambda (return)                  ; calling RETURN
        ;; O(n) where n is total number of items in both compartments
        (fold
         (lambda (char idx)
-          ;(format #t " looking at: ~a (~a)\n" char idx)
-          ;; as long as we are in the first half, remember found chars
+          ;; (format #t " looking at: ~a (~a)\n" char idx)
           (if (< idx len/2)
+              ;; as long as we are in the first half, remember found chars
               (or (vector-ref seen-chars (char->integer char))
-                  ;; as long as we are in the first half, remember found chars
                   (vector-set! seen-chars (char->integer char) #t))
 
               ;; in the second half of the list we just check for occurrences
               (if (vector-ref seen-chars (char->integer char))
                   (return (vector-ref char->priority (char->integer char)))))
-          (1+ idx))                     ; next indx
+          (1+ idx))                     ; next idx
         0                               ; start idx
         content)))))
 
-(define (get-rucksacks)
-  (string-split
-   (call-with-input-file input
-     (lambda (port)
-       (get-string-all port)))
-   #\newline))
+(define (get-group-priority rucksacks)
+  "Given a list RUCKSACKS of rucksack content strings, return their
+priority."
+  ;; This is in O(n) where n is the total length of all rucksack
+  ;; contents.
+  (let ((seen-chars (make-vector 123 0))
+        (n (length rucksacks)))
+    (call/ec
+     (lambda (return)
+       (let loop ((ruck rucksacks)
+                  (marker 1))
+         (let ((content (string->list (car ruck))))
+           (for-each
+            (lambda (char)
+              (if (= (vector-ref seen-chars (char->integer char)) (1- marker))
+                  (if (= marker n)
+                      ;; there is only one item type present in each n
+                      ;; lists, so here we can return.
+                      (return (vector-ref char->priority (char->integer char)))
+                      (vector-set! seen-chars (char->integer char) marker))))
+            content)
+           (loop (cdr ruck) (1+ marker)))
+         rucksacks)))))
+
+(define (solution-1 rucksacks)
+  (apply + (map get-priority rucksacks)))
+
+(define* (solution-2 rucksacks)
+  (assert (= (remainder (length rucksacks) 3) 0))
+  (let loop ((rest rucksacks)
+             (sum 0))
+    (match rest
+      (() sum)
+      ((r1 r2 r3 . rest)
+       (loop rest (+ sum (get-group-priority (list r1 r2 r3))))))))
+
+(define (load-rucksacks file)
+  "Processes the input file and return a list of strings, where each
+string represents the content of a single rucksack. "
+  (filter-map (lambda (line)
+                (if (string-null? line) #f line))
+   (string-split
+    (call-with-input-file file
+      (lambda (port)
+        (get-string-all port)))
+    #\newline)))
 
 (define (main args)
   (assert (= (vector-ref char->priority (char->integer #\a)) 1))
   (assert (= (vector-ref char->priority (char->integer #\z)) 26))
   (assert (= (vector-ref char->priority (char->integer #\A)) 27))
   (assert (= (vector-ref char->priority (char->integer #\Z)) 52))
-  (let ((sol1 (apply + (map get-priority (get-rucksacks)))))
+
+  (let* ((rucksacks (load-rucksacks input))
+         (sol1 (solution-1 rucksacks))
+         (sol2 (solution-2 rucksacks)))
     (assert (= sol1 7848))
-    (format #t "Solution 1: ~a\n" sol1)))
+    (assert (= sol2 2616))
+    (format #t "Solution 1: ~a\n" sol1)
+    (format #t "Solution 2: ~a\n" sol2)))
