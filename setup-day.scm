a707eae98a69e7928bef24303458e9b4c73c0286
Volker Sobek <reklov@live.com>
2022-12-26 14:57:12 +0100

    setup-day.scm: New program to set up a folder for a given day
    - Create the folder 01,...,24 according to the first command line
      argument, which must be a day number
    - Use the session Cookie stored in the file "session-cookie"
      to download the input for the day from adventofcode.com
      into input.txt inside the new folder
    - Copy skel.scm into the new folder, naming it according to the day.

---
+++ setup-day.scm
@@ -0,0 +1,46 @@
+#!/usr/local/bin/guile \
+-e main -s
+!#
+
+(use-modules (ice-9 exceptions)
+             (ice-9 format)
+             (ice-9 textual-ports)
+             (ice-9 receive)
+             (web client)
+             (web http)
+             (web response))
+
+(define aoc-skel-file "skel.scm")
+
+(define cookie-file "session-cookie")
+
+(define (main args)
+  (let ((arg1 (cdr args)))
+    (when (null? arg1)
+      (format (current-error-port) "Error: No day given.\n
+Usage: ~a DAY\nDAY is a number 1..24."
+              (car args))
+      (exit 1))
+    (let* ((day (string->number (car arg1)))
+           (number-string (format #f "~2,'0d" day))
+           (program-file (string-append number-string "/" "p" number-string ".scm"))
+           (input-file (string-append number-string "/" "input.txt")))
+      (format #t "Creating directory \"~a\"\n" number-string)
+      (mkdir number-string)
+      (format #t "Copying skel.scm to \"~a\"\n" program-file)
+      (copy-file aoc-skel-file program-file)
+      (format #t "chmod 755 ~a\n" program-file)
+      (chmod program-file #o755)
+      (let ((cookie-string (call-with-input-file cookie-file
+                             (lambda (port) (get-line port)))))
+        (receive (response body)
+            (http-get (string-append
+                       "https://adventofcode.com/2022/day/"
+                       (number->string day) "/input")
+                      #:headers `((cookie . ,cookie-string)))
+          (format #t "response: ~a\n" (response-code response))
+          (format #t "Writing input data to ~a\n" input-file)
+          (call-with-output-file input-file
+            (lambda (port)
+              (put-string port body)
+              (chmod port #o444))))))))
---
+++ skel.scm
@@ -0,0 +1,20 @@
+#!/bin/guile3.0 \
+-e main -s
+!#
+
+(use-modules (ice-9 format)
+             (ice-9 textual-ports))
+
+(define input "input.txt")
+
+
+
+(define (main args)
+  (let* ((input-file (if (null? (cdr args)) input (cadr args)))
+         (sol1 "TODO")
+         (sol2 "TODO"))
+    (when (null? (cdr args))
+      (unless (equal? sol1 "TODO") (error "Wrong solution sol1!"))
+      (unless (equal? sol2 "TODO") (error "Wrong solution sol2!")))
+    (format #t "Solution 1: ~a\n" sol1)
+    (format #t "Solution 2: ~a\n" sol2)))
