ba1d4926ddb113b7f2eb4900fb4eded96dda38cc
Volker Sobek <reklov@live.com>
2022-12-27 21:43:32 +0100

    10: Completed

--- 10/p10.scm
+++ 10/p10.scm
@@ -19,7 +19,7 @@
         (cond ((eof-object? line)
                sum)
               (else
-               (format #t "[~4'0,d] x: ~4,d strength: ~5,d ~a\n" cycle x (* cycle x) line)
+               ;; (format #t "[~4'0,d] x: ~4,d strength: ~5,d ~a\n" cycle x (* cycle x) line)
                (let ((sum-cycle (= 0 (floor-remainder (- cycle start-cycle) interval))))
                  (match (string-split line #\space)
                    (("noop")
@@ -40,13 +40,46 @@
                           (+ sum (if sum-cycle (* cycle x) 0))
                           (get-line port)))))))))))
 
+(define* (draw-screen file)
+  (call-with-input-file file
+    (lambda (port)
+      (let ((screen-width 40))
+        (let loop ((cycle 1)
+                   (x 1)                ; register value = pos of sprite center
+                   (line (get-line port)))
+          (unless (eof-object? line)
+            (let* ((crt-xpos (floor-remainder (1- cycle) screen-width))
+                   (draw (<= (abs (- crt-xpos x)) 1)))
+              (format #t "~a" (if draw "#" " "))
+              (if (= crt-xpos (1- screen-width))
+                  (format #t "\n")))
+            (match (string-split line #\space)
+              (("noop")
+               (loop (1+ cycle)
+                     x
+                     (get-line port)))
+              (("addx" n)
+               (loop (1+ cycle)
+                     x
+                     (string-join `("addx2" ,n))))
+              (("addx2" n)              ; dummy instruction
+               (loop (1+ cycle)
+                     (+ x (string->number n))
+                     (get-line port))))))))))
+
 (define (main args)
   (let* ((input-file (if (null? (cdr args)) input (cadr args)))
-         (sol1  (get-signal-strength-sum input-file 20 40))
-         (sol2 "TODO"))
-
+         (sol1 (get-signal-strength-sum input-file 20 40))
+         (sol2 (with-output-to-string (lambda () (draw-screen input-file)))))
     (when (null? (cdr args))
       (unless (= sol1 14540) (error "Wrong solution sol1!"))
-      (unless (equal? sol2 "TODO") (error "Wrong solution sol2!")))
+      (unless (equal? sol2 "\
+#### #  # #### #### #### #  #  ##  #### \n\
+#    #  #    # #       # #  # #  #    # \n\
+###  ####   #  ###    #  #### #      #  \n\
+#    #  #  #   #     #   #  # #     #   \n\
+#    #  # #    #    #    #  # #  # #    \n\
+#### #  # #### #    #### #  #  ##  #### \n")
+        (error "Wrong solution sol2!")))
     (format #t "Solution 1: ~a\n" sol1)
-    (format #t "Solution 2: ~a\n" sol2)))
+    (format #t "Solution 2:\n~a\n" sol2)))
