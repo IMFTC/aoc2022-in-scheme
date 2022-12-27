51c7409a03a9fd43c73b4160e486c77380ea30b9
Volker Sobek <reklov@live.com>
2022-12-27 20:35:10 +0100

    10: Part 1: Simplify code

--- 10/p10.scm
+++ 10/p10.scm
@@ -16,30 +16,29 @@
                  (x 1)
                  (sum 0)
                  (line (get-line port)))
-        (if (eof-object? line)
-            sum
-            (let ((sum-cycle (= 0 (floor-remainder (- cycle start-cycle) interval))))
-              (match (string-split line #\space)
-                (("noop")
-                 (format #t "[~4'0,d] x: ~4,d strength: ~5,d ~a\n" cycle x (* cycle x) line)
-                 (loop (1+ cycle)
-                       x
-                       (+ sum (if sum-cycle (* cycle x) 0))
-                       (get-line port)))
-                (("addx" n)
-                 (format #t "[~4'0,d] x: ~4,d strength: ~5,d ~a\n" cycle x (* cycle x) line)
-                 (loop (1+ cycle)
-                       x
-                       (+ sum (if sum-cycle (* cycle x) 0))
-                       ;; call a dummy command "addx2" that represents
-                       ;; the 2nd cycle of the addx command
-                       (string-join `("addx2" ,n))))
-                (("addx2" n)              ; dummy instruction
-                 (format #t "[~4'0,d] x: ~4,d strength: ~5,d ~a\n" cycle x (* cycle x) line)
-                 (loop (1+ cycle)
-                       (+ x (string->number n))
-                       (+ sum (if sum-cycle (* cycle x) 0))
-                       (get-line port))))))))))
+        (cond ((eof-object? line)
+               sum)
+              (else
+               (format #t "[~4'0,d] x: ~4,d strength: ~5,d ~a\n" cycle x (* cycle x) line)
+               (let ((sum-cycle (= 0 (floor-remainder (- cycle start-cycle) interval))))
+                 (match (string-split line #\space)
+                   (("noop")
+                    (loop (1+ cycle)
+                          x
+                          (+ sum (if sum-cycle (* cycle x) 0))
+                          (get-line port)))
+                   (("addx" n)
+                    (loop (1+ cycle)
+                          x
+                          (+ sum (if sum-cycle (* cycle x) 0))
+                          ;; call a dummy command "addx2" that represents
+                          ;; the 2nd cycle of the addx command
+                          (string-join `("addx2" ,n))))
+                   (("addx2" n)              ; dummy instruction
+                    (loop (1+ cycle)
+                          (+ x (string->number n))
+                          (+ sum (if sum-cycle (* cycle x) 0))
+                          (get-line port)))))))))))
 
 (define (main args)
   (let* ((input-file (if (null? (cdr args)) input (cadr args)))
